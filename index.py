92500d0c1ea9f54fa92682fcf723243ab5024a30
Syed Faizaan <Syed.Faizaan@vsoftwareconsulting.com>
2024-07-25 11:17:23 +0530

    eDited file enter the commit message for your changes. Lines starting

--- index.py
+++ index.py
@@ -3,3 +3,9 @@ if 2 > 5:
  print("two is greater than five!") 
 elif 5 > 2:
         print(" five is greater than two!") 
+print("Hello, World!")
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
